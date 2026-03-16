3cf8fd60db05c4e68defff26f46a9d98908f88b0
romandev <84428770+romancitodev@users.noreply.github.com>
2026-03-16 20:57:02 -0300

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src/*"),
+      },
+    },
+  },
+});
