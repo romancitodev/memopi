844547e2064b201091136bbd565756aea94a93d0
romandev <84428770+romancitodev@users.noreply.github.com>
2026-03-22 09:41:44 -0300

    ci: 💚 add ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,19 @@ import { defineConfig } from "astro/config";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import react from "@astrojs/react";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://astro.build/config
 export default defineConfig({
+  const isDeploy = process.env.GH_DEPLOY;
+  site: isDeploy ? "https://romancitodev.github.io" : undefined,
+  base: isDeploy ? "/memopi" : "/",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        "@/*": path.resolve(__dirname, "./memopi-rs/pkg/*"),
         "~/*": path.resolve(__dirname, "./src/*"),
       },
     },
   },
-
-  integrations: [react()],
 });
